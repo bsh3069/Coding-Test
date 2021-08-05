a963b82e47e36b408122d5194f27ee303648de4a
Seunghwa back <55049020+bsh3069@users.noreply.github.com>
2021-08-05 20:44:51 +0900

    Update 10798.py

--- str/10798.py
+++ str/10798.py
@@ -1,13 +1,12 @@
-# 5개의 입력
-s = [input() for i in range(5)]
-# 문자열 마다 최대 길이가 다름
-max_len = 0
-if len(s) > max_len:
-    max_len = len(s)
+# 5개의 입력           
+array = []
+for _ in range(5):
+    a= input()
+    array.append(a)
 # 이중  for문 사용
-for i in range(max_len):
-    for j in range(len(s)):
-        if i>=len(s[j]):
+for i in range(15):
+    for j in range(5):
+        if i >= len(array[j]):
             continue
         else:
-            print(s[j][i], end='')
+            print(array[j][i],end='')
